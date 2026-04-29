baf1689dd48fc9b7a521e6284927ca91b2f86d44
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2026-04-30 00:15:09 +0200

    Refactor CORS configuration to restrict allowed origins and add tests for CORS middleware

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost", "http://localhost:8000", "http://127.0.0.1", "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
